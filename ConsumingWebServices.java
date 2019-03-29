a6f38adf5e7c108200dec6a72c6741de874a701b
Bhargav Patel <47592839+iambhargi@users.noreply.github.com>
2019-03-29 14:57:12 +0530

    Update ConsumingWebServices.java

--- ConsumingWebServices.java
+++ ConsumingWebServices.java
@@ -6,16 +6,6 @@ import java.net.URL;
 import java.util.List;
 import java.util.Map;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author mca241
- */
 
 //Consuming Reqres in List and Create WebService
 
